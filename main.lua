93cb269d2fd6a2f00221f4d6e7b348bd136e7f4d
Raúl Ulises <martin.jurses@gmail.com>
2015-10-30 14:03:01 +0000

    Aprender a borrar y a insertar tablas

--- main.lua
+++ main.lua
@@ -4,15 +4,33 @@ function love.load()
 
    per= {}
    per.dim = {x = 18, y = 18}
-   per.pos = {x = 20, y = 40}
+   per.pos = {x = 80, y = 40}
    per.sal = false
    per.vel = 600
+   per.caer = true
 
    gravedad = 500
 
    suelo = {}
    suelo.dim = {x = gr.getWidth(), y = 20}
    suelo.pos = {x = 0, y = gr.getHeight() - suelo.dim.y}
+
+   enem = {}
+   enem.lado = 20
+   enem.ver = { a = {x = gr.getWidth() - enem.lado, y = suelo.pos.y},
+                b = {x = gr.getWidth(), y = suelo.pos.y},
+                c = {x = gr.getWidth() - enem.lado/2, y = suelo.pos.y - enem.lado}
+              }
+   enem.vel = 600
+
+   juego = {}
+   juego.empezar = false
+end
+
+function enemFuera(enemigo)
+   if enemigo.c.x < 0 then
+      table.remove(enemigo)
+   end
 end
 
 function checaSalto()
@@ -25,6 +43,13 @@ function colision()
    if per.pos.y + per.dim.y >= suelo.pos.y then
       return true
    end
+end
+
+function moveEnem()
+   local dt = love.timer.getDelta()
+   enem.ver.a.x = enem.ver.a.x - enem.vel * dt   
+   enem.ver.b.x = enem.ver.b.x - enem.vel * dt   
+   enem.ver.c.x = enem.ver.c.x - enem.vel * dt   
 end
 
 function salta()
@@ -43,20 +68,33 @@ function rectificador()
    end
 end
 
-function love.update(dt)
-   if checaSalto() then
-      per.pos.y = per.pos.y + gravedad * dt
-      per.sal = true
+function love.keyreleased(key)
+   if key == "g" then
+      per.caer = true
    end
+end
 
-   if colision() then
-      rectificador()
-      per.sal = false 
+function love.update(dt)
+   if per.caer then
+      if checaSalto() then
+         per.pos.y = per.pos.y + gravedad * dt
+         per.sal = true
+      end
+
+      if colision() then
+         rectificador()
+         per.sal = false 
+         per.caer = false
+         juego.empezar = true
+      end
    end
    
-   if kb.isDown("g") and not per.sal then
+   if kb.isDown("g") and not per.sal and not per.caer then
       salta()
       print("puede saltar")
+   end
+   if juego.empezar then
+      moveEnem()
    end
 end
 
@@ -69,4 +107,8 @@ function love.draw()
    else
       gr.print("No está saltando", 100,150)
    end
+
+      --ENEMIGO--
+   gr.polygon('line', enem.ver.a.x, enem.ver.a.y, enem.ver.b.x, enem.ver.b.y,
+                      enem.ver.c.x, enem.ver.c.y)
 end
